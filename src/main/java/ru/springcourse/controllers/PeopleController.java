ec48f207b45fdf464f451db0fe57ab899d87764c
anton.chachkov.qs <anton.chachkov@quantumsoft.ru>
2022-10-19 17:06:15 +0300

    Getting error "This email is already used" for User under case with creating new person with existing email is added instead of technical error of database

--- src/main/java/ru/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/springcourse/controllers/PeopleController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.springcourse.dao.PersonDAO;
 import ru.springcourse.models.Person;
+import ru.springcourse.util.PersonValidator;
 
 import javax.validation.Valid;
 
@@ -15,10 +16,12 @@ import javax.validation.Valid;
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping
@@ -43,6 +46,8 @@ public class PeopleController {
     @PostMapping
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/new";
         }
